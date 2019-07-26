02cd7c23b28bee2d6a28ac416189c7c310c016ca
Kristen Thyng <kthyng@gmail.com>
2019-07-26 14:19:58 -0500

    adding old changes

--- plot_d2_evolution.py
+++ plot_d2_evolution.py
@@ -6,13 +6,15 @@ import numpy as np
 from glob import glob
 from matplotlib import colors
 import cmocean
+import matplotlib.pyplot as plt
 
 
 # Read in dispersion calculations
-locD2 = '/home/kthyng/projects/idealized_eddies/calcs/D2/'
+locD2 = '/Volumes/COAWST/kristen/projects/idealized_eddies/calcs/D2/'
+# locD2 = '/home/kthyng/projects/idealized_eddies/calcs/D2/'
 Files = sorted(glob(locD2 + 'shelfstrat_M2_1.00e-06_N2_1.00e-04_f_1.00e-04-startday*.npz'))
 
-norm = colors.normalize(0, 1)
+norm = colors.Normalize(0, 1)
 
 # Plot
 fig = plt.figure(figsize=(10,10))
@@ -23,19 +25,19 @@ for i, File in enumerate(Files):
     d.close()
 
     frac = i/20.
-    color = cmocean.cm.temp(norm(frac))
+    color = cmocean.cm.cdom(norm(frac))
 
-    ax.semilogy(days, D2, '--', color=color, lw=4, mec=None, alpha=0.6)
+    ax.semilogy(days + i, D2, '--', color=color, lw=4, mec=None, alpha=0.6)
 
 
-shelfloc = '/rho/raid/home/kthyng/projects/shelf_transport/'
-fnameS = shelfloc + 'calcs/dispersion/hist/D2/r1/D2aveS.npz'
-D2aveS = np.load(fnameS)['D2aveS']
-dtracks = netCDF.Dataset(shelfloc + 'tracks/2004-01-01T00gc.nc')
-tp = dtracks.variables['tp']
-days2 = (tp-tp[0])/(3600.*24)
-dtracks.close()
-i25 = find(days2<=25)[-1]
-plt.semilogy(days2[:i25], D2aveS[70,45,:i25], '-', color='0.1', lw=4) # summer
+# shelfloc = '/rho/raid/home/kthyng/projects/shelf_transport/'
+# fnameS = shelfloc + 'calcs/dispersion/hist/D2/r1/D2aveS.npz'
+# D2aveS = np.load(fnameS)['D2aveS']
+# dtracks = netCDF.Dataset(shelfloc + 'tracks/2004-01-01T00gc.nc')
+# tp = dtracks.variables['tp']
+# days2 = (tp-tp[0])/(3600.*24)
+# dtracks.close()
+# i25 = find(days2<=25)[-1]
+# plt.semilogy(days2[:i25], D2aveS[70,45,:i25], '-', color='0.1', lw=4) # summer
 
-ax.set_xlim(0, 7)
+# ax.set_xlim(0, 7)
